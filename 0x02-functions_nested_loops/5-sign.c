106f7cbfb2595394274dbcbe06f29d9996d79db3
theakereleisaac <officialakisaac2018@gmail.com>
2023-02-21 14:18:59 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,11 +3,10 @@
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
- * Retun: 1 for positive num, -1 for negative num or zero for anything else
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 
 int print_sign(int n)
-
 {
 
 	if (n > 0)
